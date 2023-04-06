211141588eacefdcdee2702af569cff2946361e0
wasiushisir <wasiuahmed410@gmail.com>
2023-04-06 17:26:07 +0600

    k

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -29,7 +29,7 @@ exports.reviewUpload=async(req,res)=>{
             const newImg=new ImageModel({
                 prodId:req.body.prodId,
                 username:req.body.username,
-                revie:req.body.review,
+                revie:req.body.revie,
                 
                 image: req.file.path
                     // contentType:'image/png'
