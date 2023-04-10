6d5985bc27323def48d5fd5293d1fc1a5140dd50
wasiushisir <wasiuahmed410@gmail.com>
2023-04-10 16:54:23 +0600

    uoload none

--- controller/review.controller.js
+++ controller/review.controller.js
@@ -1,48 +1,48 @@
 
 
 const multer = require('multer')
-const path=require('path')
-const ImageModel=require('../model/review.model')
+const path = require('path')
+const ImageModel = require('../model/review.model')
 //storage
-const Storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'uploads')
+const Storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
     },
 
-    filename:(req,file,cb)=>{
+    filename: (req, file, cb) => {
         console.log(file)
-        cb(null,Date.now()+ path.extname(file.originalname))
+        cb(null, Date.now() + path.extname(file.originalname))
     }
 })
 
-const upload=multer({
-    storage:Storage
+const upload = multer({
+    storage: Storage
 }).single('testImg')
 
-exports.reviewUpload=async(req,res)=>{
-    
-    upload(req,res,(err)=>{
-        if(err){
+exports.reviewUpload = async (req, res) => {
+
+    upload.none()(req, res, (err) => {
+        if (err) {
             console.log(err)
         }
-        else{
+        else {
             console.log(req.body)
-    console.log(req.file)
-           
-            const newImg=new ImageModel({
-                prodId:req.body.prodId,
-                username:req.body.username,
-                review:req.body.review,
-                
+            console.log(req.file)
+
+            const newImg = new ImageModel({
+                prodId: req.body.prodId,
+                username: req.body.username,
+                review: req.body.review,
+
                 image: req.file.path
-                    // contentType:'image/png'
-                
+                // contentType:'image/png'
+
 
             })
             console.log(newImg)
             newImg.save()
-            .then(()=>res.status(200).json(newImg))
-            .catch((err)=>console.log(err))
+                .then(() => res.status(200).json(newImg))
+                .catch((err) => console.log(err))
         }
     })
 
@@ -52,17 +52,17 @@ exports.reviewUpload=async(req,res)=>{
 //get review
 
 
-exports.getReview=async(req,res)=>{
-    const prodId=req.query.prodId;
+exports.getReview = async (req, res) => {
+    const prodId = req.query.prodId;
     try {
-        ImageModel.find({prodId})
-        .then(data=>res.json(data))
-        .catch(err=>{
-            res.status(408).json({err})
-        })
-        
+        ImageModel.find({ prodId })
+            .then(data => res.json(data))
+            .catch(err => {
+                res.status(408).json({ err })
+            })
+
     } catch (error) {
-        res.json({error})
-        
+        res.json({ error })
+
     }
 }
